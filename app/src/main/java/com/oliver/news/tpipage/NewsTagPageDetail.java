b5da0e64b7b7efeaefae16d5d290669d9557d1b2
arrow <arrowhellish@gmail.com>
2016-11-22 11:25:07 +0800

    具体新闻界面布局设计 1. 轮播图<ViewPager> + ListView

---
+++ app/src/main/java/com/oliver/news/tpipage/NewsTagPageDetail.java
@@ -0,0 +1,25 @@
+package com.oliver.news.tpipage;
+
+import com.oliver.news.activity.HomeActivity;
+
+/**
+ * @desc 新闻数据具体显示页面
+ * @auther Oliver
+ * @time 2016/11/22 11:11
+ */
+public class NewsTagPageDetail {
+    private HomeActivity mContext;
+
+    public NewsTagPageDetail(HomeActivity context) {
+        this.mContext = context;
+
+        initView();
+    }
+
+    /**
+     * 显示数据
+     */
+    private void initView() {
+
+    }
+}
