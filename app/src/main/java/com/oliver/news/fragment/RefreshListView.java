239283a066e366b48b444492a7e88c9b8aff5a01
arrow <arrowhellish@gmail.com>
2016-11-25 17:31:46 +0800

    隐藏下拉刷新 head + 加载更多 foot

--- app/src/main/java/com/oliver/news/fragment/RefreshListView.java
+++ app/src/main/java/com/oliver/news/fragment/RefreshListView.java
@@ -16,8 +16,10 @@ import com.oliver.news.R;
 public class RefreshListView extends ListView {
 
     private LinearLayout headRoot;
-    private View refreshhead;
+    private LinearLayout mRefreshheadView;
     private View viewFoot;
+    private int mRefreshHeadHeight;
+    private int mViewFootHeight;
 
     public RefreshListView(Context context) {
         this(context, null);
@@ -39,13 +41,20 @@ public class RefreshListView extends ListView {
      */
     private void initFoot() {
         viewFoot = View.inflate(getContext(), R.layout.listview_foot, null);
+
+        /**隐藏加载更多 footer*/
+        viewFoot.measure(0, 0);
+        mViewFootHeight = viewFoot.getMeasuredHeight();
+        viewFoot.setPadding(0, -mViewFootHeight, 0, 0);
         addFooterView(viewFoot);
 
 
     }
 
 
-    /**加载轮播图
+    /**
+     * 加载轮播图
+     *
      * @param v_lunbo
      */
     public void addLunbo(View v_lunbo) {
@@ -58,8 +67,14 @@ public class RefreshListView extends ListView {
     private void initHead() {
         headRoot = (LinearLayout) View.inflate(getContext(), R.layout.listview_head, null);
 
-        refreshhead = headRoot.findViewById(R.id.ll_listview_head_refreshview);
+        mRefreshheadView = (LinearLayout) headRoot.findViewById(R.id.ll_listview_head_refreshview);
 
+
+        /**隐藏*/
+        mRefreshheadView.measure(0, 0);
+        mRefreshHeadHeight = mRefreshheadView.getMeasuredHeight();
+        /**隐藏下拉刷新头*/
+        mRefreshheadView.setPadding(0, -mRefreshHeadHeight, 0, 0);
         addHeaderView(headRoot);
     }
 }
