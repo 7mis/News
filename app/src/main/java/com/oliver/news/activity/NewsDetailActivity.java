aa3cda4b7fe0ab651dacb96c021169552d4c71da
arrow <arrowhellish@gmail.com>
2016-11-28 17:25:57 +0800

    集成 shareSDK

--- app/src/main/java/com/oliver/news/activity/NewsDetailActivity.java
+++ app/src/main/java/com/oliver/news/activity/NewsDetailActivity.java
@@ -15,6 +15,9 @@ import android.widget.ProgressBar;
 import com.oliver.news.R;
 import com.oliver.news.utils.MyConstaints;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
 /**
  * @desc 新闻显示界面
  * @auther Oliver
@@ -58,7 +61,7 @@ public class NewsDetailActivity extends AppCompatActivity {
      */
     private void initEvent() {
         /**监听数据加载完成的事件*/
-        wv_news.setWebViewClient(new WebViewClient(){
+        wv_news.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
                 /**页面加载数据完成的回调*/
@@ -98,10 +101,11 @@ public class NewsDetailActivity extends AppCompatActivity {
 
     }
 
+
     private void showTextSizeDialog() {
         AlertDialog.Builder ab = new AlertDialog.Builder(this);
         ab.setTitle("字体大小");
-        ab.setSingleChoiceItems(new String[] { "最小", "较小", "正常", "较大", "最大" },
+        ab.setSingleChoiceItems(new String[]{"最小", "较小", "正常", "较大", "最大"},
                 textSizeIndex, new DialogInterface.OnClickListener() {
 
                     @Override
@@ -145,8 +149,36 @@ public class NewsDetailActivity extends AppCompatActivity {
     }
 
     private void share() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
 
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle("标题");
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite("ShareSDK");
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+        oks.show(this);
     }
+
 
     /**
      * 界面
